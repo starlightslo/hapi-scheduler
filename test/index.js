6d348cb76e5ef52a357ac4f8a81277c5e68e4361
Tony Huang <tony@huang.ninja>
2017-12-28 10:55:12 +0800

    Add basic files.

---
+++ test/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Code = require('code');
+const Lab = require('lab');
+const { after, before, describe, it } = exports.lab = Lab.script();
+const { expect } = Code;
+
+describe('First test', () => {
+
+    before(() => {
+
+    });
+
+    it('Test', () => {
+
+        expect(true).to.be.true();
+
+    });
+
+    after(() => {
+
+    });
+
+});
