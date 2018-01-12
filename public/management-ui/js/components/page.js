4c309bc5fae74f0bf09b22ad61cd4b3c04c5168c
Tony Huang <tony@huang.ninja>
2018-01-12 12:10:08 +0800

    Adjust dropdown UI.

--- public/management-ui/js/components/page.js
+++ public/management-ui/js/components/page.js
@@ -5,9 +5,11 @@ Vue.component('Page', {
         <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             {{ nowPage }}
         </button>
-        <div class="dropdown-menu">
+        <ul class="dropdown-menu dropdown-scrollable" role="menu">
+            <li>
             <router-link class="dropdown-item" v-bind:to="path + '/' + page" v-for="page in maxPage">{{ page }}</router-link>
-        </div>
+            </li>
+        </ul>
     </div>
     `
 });
